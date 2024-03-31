7ba980af6de31dcc747654efa5b708808786f77f
Andrey <bot6923777@outlook.com>
2024-03-31 20:19:25 +0300

    готово!

--- Project.java
+++ Project.java
@@ -3,16 +3,16 @@ import java.util.Arrays;
 public class Project {
     public static void main(String[] args) {
         String[] inputArray = { "apple", "ban", "oe", "ш", "pear", "grape" };
-        String[] resultArray = filterShortStrings(inputArray);
+        String[] newArray = filterShortStrings(inputArray);
 
         System.out.println("Исходный массив: " + Arrays.toString(inputArray));
-        System.out.println("Новый массив: " + Arrays.toString(resultArray));
+        System.out.println("Новый массив: " + Arrays.toString(newArray));
     }
 
     public static String[] filterShortStrings(String[] inputArray) {
 
         // Создаем новый массив такого же размера, как исходный
-        String[] resultArray = new String[inputArray.length];
+        String[] newArray = new String[inputArray.length];
 
         int index = 0;
 
@@ -21,12 +21,12 @@ public class Project {
             // Проверяем длину текущей строки
             if (str.length() <= 3) {
                 // Если длина подходит, добавляем строку в новый массив
-                resultArray[index++] = str; // операция инкремента. Переменная index увеличивается на 1 после того, как
-                                            // ее текущее значение используется.
+                newArray[index++] = str; // Переменная index увеличивается на 1 после того, как
+                                         // ее текущее значение используется.
             }
         }
 
         // Обрезаем массив до фактического размера
-        return Arrays.copyOf(resultArray, index);
+        return Arrays.copyOf(newArray, index);
     }
 }
